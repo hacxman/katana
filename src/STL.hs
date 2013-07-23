c4ad8429df3decde16459d41d40c77ad4831b3b6
Maros Zatko <hacxman@gmail.com>
2013-07-23 14:25:43 +0200

    parser: parse in native GL types, make it 'streaming'; viever: remove older bloat, minor refactors

--- src/STL.hs
+++ src/STL.hs
@@ -15,30 +15,50 @@ import Data.Maybe
 import Data.Word (Word8)
 import Data.Char
 
+import Graphics.Rendering.OpenGL (Vertex3(..), GLfloat)
+
 import Prelude hiding (getContents)
 
 default (ByteString)
 
 data STL = STL ByteString [Facet Double] deriving Show
 data Facet d = Facet { n,x,y,z :: Point d } deriving (Show, Functor)
-data Point d = Point d d d deriving (Show, Functor)
+type Point d = Vertex3 d -- Point d d d deriving (Show, Functor)
+--instance Normal (Point Doub
 
 emptyF = Facet emptyP emptyP emptyP emptyP
-emptyP = Point 0 0 0 :: Point Double
+emptyP = Vertex3 0 0 0 :: Vertex3 GLfloat
 
-parseSTL :: Parser STL
-parseSTL = do
-  name <- string "solid " *> takeWhile1 azAZnum <* newline
-  facets <- many1 parseFacet
-  string (B.concat ["endsolid ", name]) <* newline
-  return $ STL name facets
+parseHeader :: Parser ByteString
+parseHeader = string "solid " *> takeWhile1 azAZnum <* newline
   where
     azAZnum w = (w >= 97 && w <= 121) -- a-z
              || (w >= 65 && w <= 90) -- A-Z
              || (w >= 48 && w <= 57) -- 0-9
              || (w == 95)
 
-parseFacet :: Parser (Facet Double)
+
+parseFooter :: ByteString -> Parser [Facet Double]
+parseFooter name = string (B.concat ["endsolid ", name]) *> return [] <* newline
+
+parseSTL :: BLC.ByteString -> STL
+parseSTL file =
+  let (body, name) = case parse parseHeader file of
+        Fail a b y -> error $ BLC.unpack a ++ "\n\n" ++ show b ++ "\n" ++ y
+        Done c r -> (c, r)
+      (rest, facets) = getFacets name body --many1 parseFacet
+      (_, _) = case parse (parseFooter name) rest of
+        Fail a b y -> error $ BLC.unpack a ++ "\n\n" ++ show b ++ "\n" ++ y
+        Done c r -> (c, r)
+  in STL name facets
+--  return $ STL name facets
+  where
+    getFacets name contents = case parse (parseFacet <|> parseFooter name) contents of
+      Fail a b y -> error $ BLC.unpack a ++ "\n\n" ++ show b ++ "\n" ++ y
+      Done c [] -> (c, [])
+      Done c [r] -> (c, r : (snd $ getFacets name c))
+
+parseFacet :: Parser ([Facet Double])
 parseFacet = do
   no <- (AC.skipSpace *> string "facet normal " *> parsePoint) <?> "need facet normal"
   _  <- (AC.skipSpace *> string "outer loop") <?> "need outerloop"
@@ -47,25 +67,25 @@ parseFacet = do
   zo <- AC.skipSpace *> string "vertex " *> parsePoint
   AC.skipSpace <* string "endloop\n"
   AC.skipSpace <* string "endfacet\n"
-  return $ Facet {n = no,
+  return $ [Facet {n = no,
                   x = xo,
                   y = yo,
-                  z = zo}
+                  z = zo}]
 
 parsePoint :: Parser (Point Double)
 parsePoint = do
   (n1,n2,n3) <- (,,) <$> AC.double <* AC.skipSpace
                      <*> AC.double <* AC.skipSpace
                      <*> AC.double <* newline
-  return $ Point n1 n2 n3
+  return $ Vertex3 n1 n2 n3
 
 newline :: Parser Word8
 newline = word8 10
-
-readSTL file =
+readSTL file = parseSTL file
+{-
   case parse parseSTL file of
     Fail a b y -> error $ BLC.unpack a ++ "\n\n" ++ show b ++ "\n" ++ y
-    Done c r -> r
+    Done c r -> r -}
 
 loadSTL fname = liftM readSTL $ BL.readFile fname
 
--- src/main.hs
+++ src/main.hs
@@ -9,7 +9,7 @@ import STL
 
 width = 400 :: GLfloat
 height = 400 :: GLfloat
-maxslices = 1000 :: Integer
+maxslices = 1000000 :: Integer
 
 idle = postRedisplay Nothing
 points = [(a,b) | a <- [-1,(-1+2.0/width)..1.0], b <- [-1,(-1+2.0/height)..1.0]]
@@ -17,31 +17,35 @@ points' = points `using` parListChunk 256 rdeepseq
 
 display ref model bounds = do
   qq <- readIORef ref
-  putStr $ (show qq)++"\n"
-  modifyIORef ref (+1)
+  if qq > maxslices then error "uha"
+    else do
+      putStr $ (show qq)++"\n"
+      modifyIORef ref (+1)
 
-  clearColor $= Color4 0 0.1686 0.2117 1
-  clear [ColorBuffer,DepthBuffer]
-  matrixMode $= Projection
-  -- Transformation to change the view
-  loadIdentity -- reset any transformation
-  ortho (-bounds) bounds (-bounds) bounds (0.1) bounds
-  matrixMode $= Modelview 0
-  loadIdentity
+      clearColor $= Color4 0 0.1686 0.2117 1
+      clear [ColorBuffer,DepthBuffer]
+      matrixMode $= Projection
+      -- Transformation to change the view
+      loadIdentity -- reset any transformation
+      ortho (-bounds) bounds (-bounds) bounds (0.01) bounds
+      matrixMode $= Modelview 0
+      loadIdentity
 
-  translate $ (Vector3 0 0 (realToFrac $ -bounds/2) :: Vector3 GLfloat)
---  scale 0.5 0.5 (0.5 ::GLfloat)
-  rotate (fromIntegral qq) $ (Vector3 1.0 0.0 0.0 :: Vector3 GLfloat)
-  preservingMatrix $ callList model
+      translate $ (Vector3 0 0 (realToFrac $ -bounds/2) :: Vector3 GLfloat)
+    --  scale 0.5 0.5 (0.5 ::GLfloat)
+      rotate (fromIntegral qq) $ (Vector3 1.0 0.0 0.0 :: Vector3 GLfloat)
+      preservingMatrix $ callList model
 
-  swapBuffers -- refresh screen
+      swapBuffers -- refresh screen
 
+{-
 p2v (STL.Point a b c) = Vertex3 (realToFrac a)
                                 (realToFrac b)
                                 (realToFrac c) :: Vertex3 GLfloat
 p2n (STL.Point a b c) = Normal3 (realToFrac a)
                                 (realToFrac b)
                                 (realToFrac c) :: Normal3 GLfloat
+-}
 main = do
   (prognam,args) <- getArgsAndInitialize
   initialDisplayMode $=
@@ -55,26 +59,26 @@ main = do
 --  STL n facets <- loadSTL "32ce64c4-df76-11e2-9db0-5404a640b18f.stl"
   print args
   STL n facets <- loadSTL $ args!!0
-  let model = map (\(Facet n x y z) -> (mapp x, mapp y, mapp z)) facets
-      mapp (STL.Point a b c) = Vertex3 (realToFrac a) 
-                                       (realToFrac b)
-                                       (realToFrac c) :: Vertex3 GLfloat
+  let model = map (\(Facet n x y z) -> (x, y, z)) facets
+--      mapp (STL.Point a b c) = Vertex3 (realToFrac a)
+--                                       (realToFrac b)
+--                                       (realToFrac c) :: Vertex3 GLfloat
       (aa', bb', cc') = unzip3 model
       (xx, yy, zz) = unzip3 $ map (\(Vertex3 q w e) -> (q,w,e)) (aa'++bb'++cc')
-      
       bounding_box = (maximum xx, minimum xx,
                       maximum yy, minimum yy,
                       maximum zz, minimum zz)
-      bounds (a,b,c,d,e,f) = realToFrac $ maximum [abs(a-b), abs(c-d), abs(e-f)] :: GLdouble
+      model_size = bounds bounding_box
+  --print model_size
 
   mlist <- defineNewList Compile $ do
     renderPrimitive Triangles $
       mapM_ (\(Facet n x y z) -> do
       color $ (Color3 0.5 0.4 0.7 :: Color3 GLfloat)
-      normal $ p2n n
-      vertex $ p2v x
-      vertex $ p2v y
-      vertex $ p2v z) facets
+      normal $ (\(Vertex3 a b c) -> Normal3 a b c) n
+      vertex $ x
+      vertex $ y
+      vertex $ z) facets
 
   position (Light 0) $= Vertex4 1 1 1 0
 
@@ -85,8 +89,10 @@ main = do
   la <- newIORef (0 :: Integer)
 
   idleCallback $= Just idle
-  displayCallback $= display la mlist (bounds bounding_box)
+  displayCallback $= display la mlist model_size
   blend $= Enabled
   blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
 -- We enter the main loop
   mainLoop
+  where
+    bounds (a,b,c,d,e,f) = realToFrac $ maximum [abs(a-b), abs(c-d), abs(e-f)] :: GLdouble
