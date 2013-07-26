63e5a71cc3770eca76e0d63bf9f3b506480f7173
Maros Zatko <mzatko@redhat.com>
2013-07-26 20:04:10 +0200

    add inotify support and basic command line options

--- src/main.hs
+++ src/main.hs
@@ -6,46 +6,87 @@ import Control.Parallel
 import Control.Parallel.Strategies
 import Control.Monad
 import STL
+import System.INotify
+import Control.Concurrent.STM
+import Control.Concurrent
+import Control.Concurrent.STM.TChan
+import Data.Maybe
+
+--data ProgState = ProgState {
+--    position :: Vertex3 GLfloat
+--  }
 
 width = 400 :: GLfloat
 height = 400 :: GLfloat
-maxslices = 1000000 :: Integer
+maxslices = 100 :: Integer
+
+nWatch ify fun fname = addWatch ify [Attrib] fname (
+    \e -> case e         of
+               Ignored   -> nWatch ify fun fname >> return ()
+               otherwise -> print (fname, e) >> fun fname
+    )
+
 
 idle = postRedisplay Nothing
 points = [(a,b) | a <- [-1,(-1+2.0/width)..1.0], b <- [-1,(-1+2.0/height)..1.0]]
 points' = points `using` parListChunk 256 rdeepseq
 
-display ref model bounds = do
-  qq <- readIORef ref
-  if qq > maxslices then error "uha"
-    else do
-      putStr $ (show qq)++"\n"
-      modifyIORef ref (+1)
+display ref model bounds rotax = do
+    qq <- readIORef ref
+  --if qq > maxslices then error "uha"
+  --else do
+--    putStr $ (show qq)++"\n"
+    modifyIORef ref (+1)
 
-      clearColor $= Color4 0 0.1686 0.2117 1
-      clear [ColorBuffer,DepthBuffer]
-      matrixMode $= Projection
-      -- Transformation to change the view
-      loadIdentity -- reset any transformation
-      ortho (-bounds) bounds (-bounds) bounds (0.01) bounds
-      matrixMode $= Modelview 0
-      loadIdentity
+    clearColor $= Color4 0 0.1686 0.2117 1
+    clear [ColorBuffer,DepthBuffer]
+    matrixMode $= Projection
+    -- Transformation to change the view
+    loadIdentity -- reset any transformation
+    ortho (-bounds) bounds (-bounds) bounds (-bounds) bounds
+    matrixMode $= Modelview 0
+    loadIdentity
 
-      translate $ (Vector3 0 0 (realToFrac $ -bounds/2) :: Vector3 GLfloat)
-    --  scale 0.5 0.5 (0.5 ::GLfloat)
-      rotate (fromIntegral qq) $ (Vector3 1.0 0.0 0.0 :: Vector3 GLfloat)
-      preservingMatrix $ callList model
+    translate $ (Vector3 0 0 (realToFrac $ -bounds/2) :: Vector3 GLfloat)
+    uncurry rotate rotax
+  --  scale 0.5 0.5 (0.5 ::GLfloat)
+    rotate (fromIntegral qq) $ (Vector3 1.0 0.0 0.0 :: Vector3 GLfloat)
+    rotate (fromIntegral (qq+45)) $ (Vector3 0.0 1.0 0.0 :: Vector3 GLfloat)
+    rotate (fromIntegral (qq+75)) $ (Vector3 0.0 0.0 1.0 :: Vector3 GLfloat)
+    model' <- get model
+    preservingMatrix $ callList model'
 
-      swapBuffers -- refresh screen
+    swapBuffers -- refresh screen
 
-{-
-p2v (STL.Point a b c) = Vertex3 (realToFrac a)
-                                (realToFrac b)
-                                (realToFrac c) :: Vertex3 GLfloat
-p2n (STL.Point a b c) = Normal3 (realToFrac a)
-                                (realToFrac b)
-                                (realToFrac c) :: Normal3 GLfloat
--}
+rot_args = [("-r", ( 90.0, Vector3   0.0   1.0   0.0 :: Vector3 GLfloat)),
+            ("-l", ( 90.0, Vector3   0.0 (-1.0)  0.0)),
+            ("-b", ( 90.0, Vector3   1.0   0.0   0.0)),
+            ("-t", ( 90.0, Vector3 (-1.0)  0.0   0.0)),
+            ("-bk",(180.0, Vector3   1.0   0.0   0.0)),
+            ("-f", (180.0, Vector3 (-1.0)  0.0   0.0))]
+
+modelSize facets = model_size
+  where model = map (\(Facet n x y z) -> (x, y, z)) facets
+        (aa', bb', cc') = unzip3 model
+        (xx, yy, zz) = unzip3 $ map (\(Vertex3 q w e) -> (q,w,e)) (aa'++bb'++cc')
+        bounding_box = (maximum xx, minimum xx,
+                        maximum yy, minimum yy,
+                        maximum zz, minimum zz)
+        model_size = bounds bounding_box
+        bounds (a,b,c,d,e,f) = realToFrac $
+          maximum [abs(a-b), abs(c-d), abs(e-f)] :: GLdouble
+
+displistFromFacets facets = do
+  mlist' <- defineNewList Compile $ do
+    renderPrimitive Triangles $
+      mapM_ (\(Facet n x y z) -> do
+      color $ (Color3 0.5 0.4 0.7 :: Color3 GLfloat)
+      normal $ (\(Vertex3 a b c) -> Normal3 a b c) n
+      vertex $ x
+      vertex $ y
+      vertex $ z) facets
+  return mlist'
+
 main = do
   (prognam,args) <- getArgsAndInitialize
   initialDisplayMode $=
@@ -56,29 +97,14 @@ main = do
   depthFunc  $= Just Less
   windowSize $= Size (round width) (round height)
 
---  STL n facets <- loadSTL "32ce64c4-df76-11e2-9db0-5404a640b18f.stl"
   print args
   STL n facets <- loadSTL $ args!!0
-  let model = map (\(Facet n x y z) -> (x, y, z)) facets
---      mapp (STL.Point a b c) = Vertex3 (realToFrac a)
---                                       (realToFrac b)
---                                       (realToFrac c) :: Vertex3 GLfloat
-      (aa', bb', cc') = unzip3 model
-      (xx, yy, zz) = unzip3 $ map (\(Vertex3 q w e) -> (q,w,e)) (aa'++bb'++cc')
-      bounding_box = (maximum xx, minimum xx,
-                      maximum yy, minimum yy,
-                      maximum zz, minimum zz)
-      model_size = bounds bounding_box
-  --print model_size
 
-  mlist <- defineNewList Compile $ do
-    renderPrimitive Triangles $
-      mapM_ (\(Facet n x y z) -> do
-      color $ (Color3 0.5 0.4 0.7 :: Color3 GLfloat)
-      normal $ (\(Vertex3 a b c) -> Normal3 a b c) n
-      vertex $ x
-      vertex $ y
-      vertex $ z) facets
+  let model_size = modelSize facets
+  print model_size
+
+  mlist' <- displistFromFacets facets
+  mlist <- newIORef mlist'
 
   position (Light 0) $= Vertex4 1 1 1 0
 
@@ -89,10 +115,25 @@ main = do
   la <- newIORef (0 :: Integer)
 
   idleCallback $= Just idle
-  displayCallback $= display la mlist model_size
+  displayCallback $= display la mlist model_size (fromJust $ lookup (args!!1) rot_args)
   blend $= Enabled
   blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
+
+  keyboardMouseCallback $= Just (keyboard pPos)
+
+  ify <- initINotify
+  w0 <- nWatch ify (reloader mlist) $ args!!0
+--    \fname -> (forkIO $ reloader mlist fname) >> (return ())) $ args!!0
+
 -- We enter the main loop
   mainLoop
   where
-    bounds (a,b,c,d,e,f) = realToFrac $ maximum [abs(a-b), abs(c-d), abs(e-f)] :: GLdouble
+    reloader mlist fname = do
+      STL n facets <- loadSTL fname
+      displist <- displistFromFacets facets
+      print $ modelSize facets
+      finish
+      lst <- get mlist
+      deleteObjectNames [lst]
+      mlist $= displist
+      print ("FUHA", n)
